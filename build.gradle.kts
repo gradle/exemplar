5040c2f3d72018e97babea9d536cb84a83a7460f
Eric Wendelin <eric@gradle.com>
2018-08-23 09:46:49 -0700

    Update version to 0.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "org.gradle"
-    version = "0.5.0"
+    version = "0.5.1"
 
     configure<PublishingExtension> {
         publications.create<MavenPublication>("mavenJava") {
