0b1da77395bcae64f2ac9d39c2f6ee5f2982a382
StefMa <StefMaDev@outlook.com>
2018-10-12 11:48:29 +0200

    Add TODOs and FIXME

--- plugin/src/main/kotlin/org/gradle/exemplar/DependencyHandlerExtension.kt
+++ plugin/src/main/kotlin/org/gradle/exemplar/DependencyHandlerExtension.kt
@@ -3,9 +3,12 @@ package org.gradle.exemplar
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 internal fun DependencyHandler.sampleCheck() =
+    // TODO: We have to inject the project-version into it and replace the hardcoded "0.6.1"
+    // how can we do that?
     add("testImplementation", "org.gradle:sample-check:0.6.1")
 
 fun DependencyHandler.gradleExemplar() {
     add("testImplementation", gradleTestKit())
+    // FIXME: Hardcoding of the version is a bad idea.. Try to change this.. somehow.
     add("testRuntimeOnly", "org.slf4j:slf4j-simple:1.7.16")
 }
