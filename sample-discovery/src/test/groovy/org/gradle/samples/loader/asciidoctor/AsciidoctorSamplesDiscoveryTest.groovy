06e8e34c8d93879fcfa181e69a63d18f3f71376b
Daniel Lacasse <daniel@gradle.com>
2020-01-08 22:19:51 +0400

    Formating

--- sample-discovery/src/test/groovy/org/gradle/samples/loader/asciidoctor/AsciidoctorSamplesDiscoveryTest.groovy
+++ sample-discovery/src/test/groovy/org/gradle/samples/loader/asciidoctor/AsciidoctorSamplesDiscoveryTest.groovy
@@ -28,31 +28,31 @@ class AsciidoctorSamplesDiscoveryTest extends Specification {
 
     def "discovers samples inside an asciidoctor file with sources inline"() {
         given:
-        def file = tmpDir.newFile("sample.adoc") << """
-= Document Title
-
-.Sample title
-====
-[.testable-sample]
-=====
-.hello.rb
-[source,ruby]
-----
-target = "world"
-puts "hello, #{target}"
-----
-
-[.sample-command,allow-disordered-output=true]
-----
-\$ ruby hello.rb
-
-hello, world
-
-some more output
-----
-=====
-====
-"""
+        def file = tmpDir.newFile('sample.adoc') << '''
+            |= Document Title
+            |
+            |.Sample title
+            |====
+            |[.testable-sample]
+            |=====
+            |.hello.rb
+            |[source,ruby]
+            |----
+            |target = "world"
+            |puts "hello, #{target}"
+            |----
+            |
+            |[.sample-command,allow-disordered-output=true]
+            |----
+            |$ ruby hello.rb
+            |
+            |hello, world
+            |
+            |some more output
+            |----
+            |=====
+            |====
+            |'''.stripMargin()
 
         when:
         Collection<Sample> samples = AsciidoctorSamplesDiscovery.extractFromAsciidoctorFile(file)
@@ -64,86 +64,86 @@ some more output
         and:
         commands.size() == 1
         def command = commands.get(0)
-        command.executable == "ruby"
-        command.args == ["hello.rb"]
+        command.executable == 'ruby'
+        command.args == ['hello.rb']
         command.allowDisorderedOutput
-        command.expectedOutput == """
-hello, world
-
-some more output"""
+        command.expectedOutput == '''
+            |hello, world
+            |
+            |some more output'''.stripMargin()
     }
 
     def "discovers samples inside an asciidoctor file with sources included"() {
         given:
-        tmpDir.newFolder("src", "samples", "bash")
-        tmpDir.newFile("src/samples/bash/script.sh") << """
-#!/usr/bin/env bash
-
-echo "Hello world"
-"""
-        def file = tmpDir.newFile("sample.adoc") << """
-= Document title
-
-.Sample title
-====
-[.testable-sample,dir="src/samples/bash"]
-=====
-.script.sh
-[source,bash]
-----
-include::src/samples/bash/script.sh[]
-----
-
-[.sample-command]
-----
-\$ bash script.sh
-Hello world
-----
-=====
-====
-"""
+        tmpDir.newFolder('src', 'samples', 'bash')
+        tmpDir.newFile('src/samples/bash/script.sh') << '''
+            |#!/usr/bin/env bash
+            |
+            |echo "Hello world"
+            |'''.stripMargin()
+        def file = tmpDir.newFile('sample.adoc') << '''
+            |= Document title
+            |
+            |.Sample title
+            |====
+            |[.testable-sample,dir="src/samples/bash"]
+            |=====
+            |.script.sh
+            |[source,bash]
+            |----
+            |include::src/samples/bash/script.sh[]
+            |----
+            |
+            |[.sample-command]
+            |----
+            |$ bash script.sh
+            |Hello world
+            |----
+            |=====
+            |====
+            |'''.stripMargin()
 
         when:
         Collection<Sample> samples = AsciidoctorSamplesDiscovery.extractFromAsciidoctorFile(file)
 
         then:
         samples.size() == 1
-        samples.get(0).projectDir.toString() == "src/samples/bash"
+        samples.get(0).projectDir.toString() == 'src/samples/bash'
         def commands = samples.get(0).commands
 
         and:
         commands.size() == 1
         def command = commands.get(0)
-        command.executable == "bash"
-        command.args == ["script.sh"]
-        command.expectedOutput == "Hello world"
+        command.executable == 'bash'
+        command.args == ['script.sh']
+        command.expectedOutput == 'Hello world'
     }
 
     def "sample may include multiple sample-command blocks"() {
         given:
-        def file = tmpDir.newFile("sample.adoc") << """
-= Document Title
-
-.Sample title
-[.testable-sample]
-====
-
-Run this first:
-
-[.sample-command]
-----
-\$ ruby hello.rb
-hello, world
-----
-
-Then do this:
-
-[.sample-command]
-----
-\$ mkdir some-dir
-----
-====
-"""
+        def file = tmpDir.newFile('sample.adoc') << '''
+            |= Document Title
+            |
+            |.Sample title
+            |[.testable-sample]
+            |====
+            |
+            |Run this first:
+            |
+            |[.sample-command]
+            |----
+            |$ ruby hello.rb
+            |hello, world
+            |----
+            |
+            |Then do this:
+            |
+            |[.sample-command]
+            |----
+            |$ mkdir some-dir
+            |----
+            |====
+            |'''.stripMargin()
 
         when:
         Collection<Sample> samples = AsciidoctorSamplesDiscovery.extractFromAsciidoctorFile(file)
@@ -155,38 +155,38 @@ Then do this:
         and:
         commands.size() == 2
         def command = commands.get(0)
-        command.executable == "ruby"
-        command.args == ["hello.rb"]
-        command.expectedOutput == "hello, world"
+        command.executable == 'ruby'
+        command.args == ['hello.rb']
+        command.expectedOutput == 'hello, world'
 
         def command2 = commands.get(1)
-        command2.executable == "mkdir"
-        command2.args == ["some-dir"]
+        command2.executable == 'mkdir'
+        command2.args == ['some-dir']
         command2.expectedOutput.empty
     }
 
     def "sample-command block may include multiple commands"() {
         given:
-        def file = tmpDir.newFile("sample.adoc") << """
-= Document Title
-
-.Sample title
-[.testable-sample]
-====
-
-Run this first:
-
-[.sample-command]
-----
-\$ ruby hello.rb
-
-hello, world
-
-\$ mkdir some-dir
-\$ cd some-dir
-----
-====
-"""
+        def file = tmpDir.newFile('sample.adoc') << '''
+            |= Document Title
+            |
+            |.Sample title
+            |[.testable-sample]
+            |====
+            |
+            |Run this first:
+            |
+            |[.sample-command]
+            |----
+            |$ ruby hello.rb
+            |
+            |hello, world
+            |
+            |$ mkdir some-dir
+            |$ cd some-dir
+            |----
+            |====
+            |'''.stripMargin()
 
         when:
         Collection<Sample> samples = AsciidoctorSamplesDiscovery.extractFromAsciidoctorFile(file)
@@ -201,23 +201,23 @@ hello, world
         command.executable == "ruby"
         command.args == ["hello.rb"]
         command.expectedOutput == """
-hello, world
-"""
+            |hello, world
+            |""".stripMargin()
 
         def command2 = commands.get(1)
-        command2.executable == "mkdir"
-        command2.args == ["some-dir"]
+        command2.executable == 'mkdir'
+        command2.args == ['some-dir']
         command2.expectedOutput.empty
 
         def command3 = commands.get(2)
-        command3.executable == "cd"
-        command3.args == ["some-dir"]
+        command3.executable == 'cd'
+        command3.args == ['some-dir']
         command3.expectedOutput.empty
     }
 
     def "can include data from attributes"() {
         given:
-        def file = tmpDir.newFile("sample.adoc") << """
+        def file = tmpDir.newFile('sample.adoc') << '''
             |= Document Title
             |
             |.Sample title
@@ -228,11 +228,11 @@ hello, world
             |
             |[listing.terminal.sample-command]
             |----
-            |\$ pwd
+            |$ pwd
             |include::{sampleoutputdir}/pwd-output.txt[]
             |----
             |====
-            |""".stripMargin()
+            |'''.stripMargin()
         def outputDir = tmpDir.newFolder('output')
         tmpDir.newFile('output/pwd-output.txt').text = "${tmpDir.root.getAbsolutePath()}\n"
 
@@ -248,14 +248,14 @@ hello, world
         and:
         commands.size() == 1
         def command = commands.get(0)
-        command.executable == "pwd"
+        command.executable == 'pwd'
         command.args == []
         command.expectedOutput == tmpDir.root.getAbsolutePath()
     }
 
     def "can extract commands when using Asciidoctor callout"() {
         given:
-        def file = tmpDir.newFile("sample.adoc") << '''
+        def file = tmpDir.newFile('sample.adoc') << '''
             |= Document Title
             |
             |[listing.terminal.testable-sample.sample-command]
@@ -276,7 +276,7 @@ hello, world
         and:
         commands.size() == 1
         def command = commands.get(0)
-        command.executable == "./command"
+        command.executable == './command'
         command.args == []
         command.expectedOutput == 'Some output // <1>'
     }
