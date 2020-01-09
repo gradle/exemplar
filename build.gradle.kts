e1e489e146f2b9cdd3db3c87734bc4c0384299c3
Daniel Lacasse <daniel@gradle.com>
2020-01-09 15:15:40 +0400

    Release version 0.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "org.gradle"
-    version = "0.12.0"
+    version = "0.12.1"
 
     configure<PublishingExtension> {
         publications.create<MavenPublication>("mavenJava") {
