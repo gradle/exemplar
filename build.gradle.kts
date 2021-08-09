e991835818fff3747c608dff566c666a2a593a30
Przemek Bielicki <przemek@gradle.com>
2021-08-09 15:14:22 +0200

    Apply Nexus publish plugin to publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,16 @@
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
 version = "0.12.6"
+
+nexusPublishing {
+    repositories.apply {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("MAVEN_CENTRAL_STAGING_REPO_USER"))
+            password.set(System.getenv("MAVEN_CENTRAL_STAGING_REPO_PASSWORD"))
+        }
+    }
+}
