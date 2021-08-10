f13d8023ae8bdd88f1121a649d9d6a8e6dbe1c19
Przemyslaw Bielicki <przemek@gradle.com>
2021-08-10 16:40:49 +0200

    Update settings.gradle.kts
    Co-authored-by: Benedikt Ritter <benedikt@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.support.serviceOf
-
 plugins {
     id("com.gradle.enterprise").version("3.6.1")
     id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.7.2")
