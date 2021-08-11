b8b5e749b690300e29d18629734e0cf7d3e20cf2
Przemek Bielicki <przemek@gradle.com>
2021-08-11 12:26:18 +0200

    Add missing information required by Maven Central

--- buildSrc/src/main/kotlin/exemplar.publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/exemplar.publishing-conventions.gradle.kts
@@ -10,6 +10,30 @@ publishing {
     publications.create<MavenPublication>("mavenJava") {
         artifactId = project.name
         from(components["java"])
+
+        pom {
+            name.set("Exemplar ${project.name}")
+            description.set("Given a collection of sample projects, this library allows you to verify the samples' output.")
+            url.set("https://github.com/gradle/exemplar")
+            licenses {
+                license {
+                    name.set("Apache-2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                }
+            }
+            developers {
+                developer {
+                    name.set("The Gradle team")
+                    organization.set("Gradle Inc.")
+                    organizationUrl.set("https://gradle.com")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/gradle/exemplar.git")
+                developerConnection.set("scm:git:ssh://git@github.com:gradle/exemplar.git")
+                url.set("https://github.com/gradle/exemplar")
+            }
+        }
     }
 }
 
