ca3e2e0caf16823f8111088e2c4285a4a3f56628
Przemek Bielicki <przemek@gradle.com>
2021-10-06 16:48:04 +0200

    Set test class name correctly

--- samples-check/src/main/java/org/gradle/exemplar/test/engine/SamplesRunnerJUnitEngine.java
+++ samples-check/src/main/java/org/gradle/exemplar/test/engine/SamplesRunnerJUnitEngine.java
@@ -11,10 +11,10 @@ import org.gradle.exemplar.model.Command;
 import org.gradle.exemplar.model.Sample;
 import org.gradle.exemplar.test.normalizer.OutputNormalizer;
 import org.gradle.exemplar.test.runner.SampleModifier;
+import org.gradle.exemplar.test.runner.SamplesRoot;
 import org.gradle.exemplar.test.runner.SamplesRunner;
 import org.gradle.exemplar.test.verifier.AnyOrderLineSegmentedOutputVerifier;
 import org.gradle.exemplar.test.verifier.StrictOrderLineSegmentedOutputVerifier;
-import org.junit.Assert;
 import org.junit.platform.engine.EngineDiscoveryRequest;
 import org.junit.platform.engine.EngineExecutionListener;
 import org.junit.platform.engine.ExecutionRequest;
@@ -22,8 +22,11 @@ import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.engine.TestEngine;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.UniqueId;
+import org.junit.platform.engine.discovery.ClassSelector;
 import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor;
+import org.junit.platform.engine.support.descriptor.ClassSource;
 import org.junit.platform.engine.support.descriptor.EngineDescriptor;
+import org.opentest4j.AssertionFailedError;
 
 import java.io.File;
 import java.io.IOException;
@@ -52,9 +55,27 @@ public class SamplesRunnerJUnitEngine implements TestEngine {
     @Override
     public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
         EngineDescriptor engineDescriptor = new EngineDescriptor(uniqueId, "Samples Runner JUnit Engine");
-        for (Sample sample : SamplesDiscovery.externalSamples(new File("src/docs/snippets"))) {
-            engineDescriptor.addChild(new SampleTestDescriptor(sample));
-        }
+        discoveryRequest.getSelectorsByType(ClassSelector.class)
+                .stream()
+                .map(ClassSelector::getJavaClass).forEach(javaClass -> {
+                    SamplesRoot samplesRoot = javaClass.getAnnotation(SamplesRoot.class);
+                    if (samplesRoot != null) {
+                        SamplesTestDescriptor samplesTestDescriptor = new SamplesTestDescriptor(
+                                uniqueId.append("className", javaClass.getSimpleName()),
+                                javaClass
+                        );
+
+                        for (Sample sample : SamplesDiscovery.externalSamples(getSamplesRootDir(samplesRoot))) {
+                            samplesTestDescriptor.addChild(new SampleTestDescriptor(
+                                    uniqueId.append("sampleId", sample.getId()),
+                                    sample
+                            ));
+                        }
+
+                        engineDescriptor.addChild(samplesTestDescriptor);
+                    }
+                });
+
         return engineDescriptor;
     }
 
@@ -64,19 +85,40 @@ public class SamplesRunnerJUnitEngine implements TestEngine {
         EngineExecutionListener listener = request.getEngineExecutionListener();
 
         listener.executionStarted(engineDescriptor);
-        for (TestDescriptor testDescriptor : engineDescriptor.getChildren()) {
-            SampleTestDescriptor descriptor = (SampleTestDescriptor) testDescriptor;
-            listener.executionStarted(testDescriptor);
-
-            try {
-                runSample(descriptor.sample);
-                listener.executionFinished(testDescriptor, TestExecutionResult.successful());
-            } catch (Throwable t) {
-                listener.executionFinished(testDescriptor, TestExecutionResult.failed(t));
+        for (TestDescriptor classDescriptor : engineDescriptor.getChildren()) {
+            listener.executionStarted(classDescriptor);
+            for (TestDescriptor testDescriptor : classDescriptor.getChildren()) {
+                SampleTestDescriptor descriptor = (SampleTestDescriptor) testDescriptor;
+                listener.executionStarted(testDescriptor);
+                try {
+                    runSample(descriptor.sample);
+                    listener.executionFinished(testDescriptor, TestExecutionResult.successful());
+                } catch (Throwable t) {
+                    listener.executionFinished(testDescriptor, TestExecutionResult.failed(t));
+                }
             }
 
+            listener.executionFinished(classDescriptor, TestExecutionResult.successful());
         }
         listener.executionFinished(engineDescriptor, TestExecutionResult.successful());
+    }
+
+    protected File getSamplesRootDir(SamplesRoot samplesRoot) {
+        File samplesRootDir = null;
+        try {
+            if (samplesRoot != null) {
+                samplesRootDir = new File(samplesRoot.value());
+            }
+            if (samplesRootDir == null) {
+                throw new IllegalArgumentException("Samples root directory is not declared. Please annotate your test class with @SamplesRoot(\"path/to/samples\")");
+            }
+            if (!samplesRootDir.exists()) {
+                throw new IllegalArgumentException("Samples root directory " + samplesRootDir.getAbsolutePath() + " does not exist");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not initialize SamplesRunnerJUnitEngine", e);
+        }
+        return samplesRootDir;
     }
 
     private void runSample(Sample sample) throws Exception {
@@ -100,9 +142,21 @@ public class SamplesRunnerJUnitEngine implements TestEngine {
             CommandExecutionResult result = executeSample(getExecutionMetadata(testSpecificSample.getProjectDir()), workingDir, command);
 
             if (result.getExitCode() != 0 && !command.isExpectFailure()) {
-                Assert.fail(String.format("Expected sample invocation to succeed but it failed.%nCommand was: '%s %s'%nWorking directory: '%s'%n[BEGIN OUTPUT]%n%s%n[END OUTPUT]%n", command.getExecutable(), StringUtils.join(command.getArgs(), " "), workingDir.getAbsolutePath(), result.getOutput()));
+                throw new AssertionFailedError(String.format(
+                        "Expected sample invocation to succeed but it failed.%nCommand was: '%s %s'%nWorking directory: '%s'%n[BEGIN OUTPUT]%n%s%n[END OUTPUT]%n",
+                        command.getExecutable(),
+                        StringUtils.join(command.getArgs(), " "),
+                        workingDir.getAbsolutePath(),
+                        result.getOutput()
+                ));
             } else if (result.getExitCode() == 0 && command.isExpectFailure()) {
-                Assert.fail(String.format("Expected sample invocation to fail but it succeeded.%nCommand was: '%s %s'%nWorking directory: '%s'%n[BEGIN OUTPUT]%n%s%n[END OUTPUT]%n", command.getExecutable(), StringUtils.join(command.getArgs(), " "), workingDir.getAbsolutePath(), result.getOutput()));
+                throw new AssertionFailedError(String.format(
+                        "Expected sample invocation to fail but it succeeded.%nCommand was: '%s %s'%nWorking directory: '%s'%n[BEGIN OUTPUT]%n%s%n[END OUTPUT]%n",
+                        command.getExecutable(),
+                        StringUtils.join(command.getArgs(), " "),
+                        workingDir.getAbsolutePath(),
+                        result.getOutput()
+                ));
             }
 
             verifyOutput(command, result);
@@ -159,14 +213,26 @@ public class SamplesRunnerJUnitEngine implements TestEngine {
 
         private final Sample sample;
 
-        private SampleTestDescriptor(Sample sample) {
-            super(UniqueId.forEngine("SamplesRunnerJUnitEngine" + sample.getId()), sample.getId());
+        private SampleTestDescriptor(UniqueId uniqueId, Sample sample) {
+            super(uniqueId, sample.getId());
             this.sample = sample;
         }
 
         @Override
         public Type getType() {
             return Type.TEST;
+        }
+    }
+
+    private static class SamplesTestDescriptor extends AbstractTestDescriptor {
+
+        private SamplesTestDescriptor(UniqueId uniqueId, Class<?> testClass) {
+            super(uniqueId, testClass.getSimpleName(), ClassSource.from(testClass));
+        }
+
+        @Override
+        public Type getType() {
+            return Type.CONTAINER;
         }
     }
 }
