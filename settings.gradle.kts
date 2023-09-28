386ae5b14bd4d86d08b2f7f1dd9b27c6321d6ad6
Przemek Bielicki <przemek@gradle.com>
2023-09-28 17:23:22 +0200

    Upgrade Gradle wrapper to version 8.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("com.gradle.enterprise.gradle-enterprise-conventions-plugin") version "0.7.2"
 }
 
-enableFeaturePreview("VERSION_CATALOGS")
-
 rootProject.name = "exemplar"
 
 include("samples-discovery")
